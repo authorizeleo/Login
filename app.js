f7a27228cbfdbf964bb36a5a82a908c5db1bb72e
Leo Gao <skyto122010@gmail.com>
2021-04-13 14:10:29 +0800

    mongodb connect

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app  = express()
 const port = 3000
+
+mongoose.connect('mongodb://localhost/todo-list', {useNewUrlParser: true , useUnifiedTopology: true})
+
+const db = mongoose.connection
+
+db.on('error', () => {
+    console.log('error notice')
+})
+
+db.once('open', () => {
+    console.log('good connected')
+})
+
 
 app.get('/', (req, res) => {
     res.send('test1')
