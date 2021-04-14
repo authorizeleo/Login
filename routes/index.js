eedf98f6b7030822cfd4c73850bf279681acda45
Leo Gao <skyto122010@gmail.com>
2021-04-14 13:45:25 +0800

    first middleware

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,14 @@ const router = express.Router()
 const home = require('./modules/home')
 const tofunction = require('./modules/tofunction')
 const login = require('./modules/login')
+const { authenticator } = require('../middleware/auth')
 
 
-router.use('/', home)
-router.use('/todo', tofunction)
+
+
 router.use('/user', login)
+// 為了 不被驗證擋下 所以 放在最下面
+router.use('/todo', authenticator, tofunction)
+router.use('/', authenticator, home)
 
 module.exports = router
