f0b2098e12b78a5d68b7304207fa7fe157696e5f
Leo Gao <skyto122010@gmail.com>
2021-04-14 10:21:14 +0800

    first login UI

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,11 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const tofunction = require('./modules/tofunction')
+const login = require('./modules/login')
+
 
 router.use('/', home)
 router.use('/todo', tofunction)
+router.use('/login', login)
 
 module.exports = router
---
+++ views/user.hbs
@@ -0,0 +1,1 @@
+<h3>login test</h3>
